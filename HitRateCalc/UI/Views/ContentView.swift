56b048b95ff4f6b98db4ce68268cdcfa6b43450b
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-01-12 14:49:41 -0800

    added toggling animation to switch TraitSections positions

--- HitRateCalc/UI/Views/ContentView.swift
+++ HitRateCalc/UI/Views/ContentView.swift
@@ -10,18 +10,43 @@ import SwiftUI
 struct ContentView: View {
     @StateObject var dataModel = HitRateDataModel()
     
+    private var offset: CGFloat { getHeightPercent(20) }
+    private var accuracyOffset: CGFloat { dataModel.checkingHitRate ? -offset : offset }
+    private var evasionOffset: CGFloat { dataModel.checkingHitRate ? offset : -offset }
     private var evasionRate: String { "\(dataModel.evasionRate)" }
     private var accuracyRate: String { "\(dataModel.accuracyRate)" }
+    
+    private func toggleMode() {
+        withAnimation { dataModel.toggleMode() }
+    }
     
     var body: some View {
         NavigationView {
             VStack {
                 Divider()
                 
-                TraitsSection(traitList: $dataModel.evasionTraits, title: "Evasion", rateResult: evasionRate)
+                VStack {
+                    ZStack {
+                        TraitsSection(traitList: $dataModel.evasionTraits, title: "Evasion", rateResult: evasionRate)
+                            .offset(y: evasionOffset)
+                        
+                        Button(action: toggleMode) {
+                            Image(systemName: "arrow.up.arrow.down")
+                                .font(.largeTitle)
+                                .cornerRadius(1)
+                        }
+                        .tint(.black)
+                        .buttonStyle(.borderedProminent)
+                        .padding(.top, getHeightPercent(5))
+                        .shadow(color: .primary, radius: 4)
+                        
+                        TraitsSection(traitList: $dataModel.accuracyTraits, title: "Accuracy", rateResult: accuracyRate)
+                            .offset(y: accuracyOffset)
+                            
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
                 
-                TraitsSection(traitList: $dataModel.accuracyTraits, title: "Accuracy", rateResult: accuracyRate)
-                
+                Spacer()
                 VStack(spacing: 0) {
                     Text(dataModel.finalRateTitle)
                     Text(dataModel.finalRate)
@@ -72,6 +97,8 @@ extension HitRateDataModel {
         
         return HitRateCalculator.getAccuracyRate(dex: accAmounts[0], luck: accAmounts[1], bonus: accAmounts[2])
     }
+    
+    func toggleMode() { checkingHitRate.toggle() }
 }
 
 private extension HitRateDataModel {
