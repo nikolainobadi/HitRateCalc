f1a8ed660e013362d6e72349504c06fc57ee5d9f
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-01-12 12:10:36 -0800

    Organized files and added AppLauncher to make unit tests run smoother

---
+++ HitRateCalc/Main/HitRateCalcApp.swift
@@ -0,0 +1,39 @@
+//
+//  HitRateCalcApp.swift
+//  HitRateCalc
+//
+//  Created by Nikolai Nobadi on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppLauncher {
+    static func main() throws {
+        if NSClassFromString("XCTestCase") == nil {
+            HitRateCalcApp.main()
+        } else {
+            TestApp.main()
+        }
+    }
+}
+
+struct HitRateCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Running Unit Tests")
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(.black)
+        }
+    }
+}
+
