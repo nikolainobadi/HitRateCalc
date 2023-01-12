58f80fb57f9aed89ea2ca191ec46df84385535ff
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-01-12 12:07:03 -0800

    Initial Commit

---
+++ HitRateCalc/HitRateCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  HitRateCalcApp.swift
+//  HitRateCalc
+//
+//  Created by Nikolai Nobadi on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HitRateCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
