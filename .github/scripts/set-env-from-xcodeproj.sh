6c89ba3ee72c0e025b01080e5284807a090ee39a
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-01-12 19:15:48 -0800

    experimenting with a different version of set-env-from-xcode

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
-SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
-PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
-echo "PRODUCT_NAME=$PRODUCT_NAME" >> $GITHUB_ENV
+#SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
+#PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
+#echo "PRODUCT_NAME=$PRODUCT_NAME" >> $GITHUB_ENV
+
+PROJECT_NAME=$(xcodebuild -list -project MyProject.xcodeproj|grep -o 'MyProject')
+SCHEME=$(xcodebuild -list -project MyProject.xcodeproj|grep -o 'MyScheme')
+export PROJECT_NAME
+export SCHEME
