fa0c5b72a779c4d5cab2d797cf6973735331afc6
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-13 09:45:12 -0700

    moved StatsContainerInfo to its own file

--- HitRateCalc/UI/Views/VisionStatsContainer.swift
+++ HitRateCalc/UI/Views/VisionStatsContainer.swift
@@ -70,35 +70,3 @@ struct VisionStatsContainer_Previews: PreviewProvider {
         VisionStatsContainer(viewModel: .accuracy(Vision()))
     }
 }
-
-
-// MARK: - Dependencies
-enum StatsContainerInfo {
-    case evasion(Vision)
-    case accuracy(Vision)
-}
-
-extension StatsContainerInfo {
-    var title: String {
-        switch self {
-        case .evasion: return "Evasion"
-        case .accuracy: return "Accuracy"
-        }
-    }
-    
-    var statList: [VisionStat] {
-        switch self {
-        case .evasion(let vision): return vision.evadeStats
-        case .accuracy(let vision): return vision.accuracyStats
-        }
-    }
-    
-    var statRate: Int {
-        switch self {
-        case .evasion(let vision):
-            return HitRateCalculator.getAccuracyRate(for: vision)
-        case .accuracy(let vision):
-            return HitRateCalculator.getAccuracyRate(for: vision)
-        }
-    }
-}
