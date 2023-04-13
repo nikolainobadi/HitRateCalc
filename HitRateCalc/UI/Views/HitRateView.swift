8d39d00d5a93c9d769983bfd990f378a1ea7ac49
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-13 10:09:09 -0700

    added new HitRateView file

---
+++ HitRateCalc/UI/Views/HitRateView.swift
@@ -0,0 +1,28 @@
+//
+//  HitRateView.swift
+//  HitRateCalc
+//
+//  Created by Nikolai Nobadi on 4/13/23.
+//
+
+import SwiftUI
+
+struct HitRateView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+            }
+            .navigationTitle("Hit-Rate Calc")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+
+// MARK: - Preview
+struct HitRateView_Previews: PreviewProvider {
+    static var previews: some View {
+        HitRateView()
+    }
+}
