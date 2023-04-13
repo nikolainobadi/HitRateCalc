3171e7200067d262e3acfa3d012ba1d6cebaf03b
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-13 12:01:26 -0700

    added new HitRatCoordinatorView

---
+++ HitRateCalc/Main/HitRateCoordinatorView.swift
@@ -0,0 +1,62 @@
+//
+//  HitRateCoordinatorView.swift
+//  HitRateCalc
+//
+//  Created by Nikolai Nobadi on 4/13/23.
+//
+
+import SwiftUI
+
+struct HitRateCoordinatorView: View {
+    @StateObject private var dataModel = HitRateDataModel()
+    
+    var body: some View {
+        NavigationStack {
+            HitRateView(dataModel: dataModel)
+                .navigationTitle("Hit-Rate Calc")
+                .navigationBarTitleDisplayMode(.inline)
+                .sheet(item: $dataModel.selectedInfo) { info in
+                    NavigationStack {
+                        VisionDetailsView(dataModel: makeDataModel(info, completion: { updatedVision in
+                            updateVision(vision: updatedVision, info: info)
+                            dataModel.selectedInfo = nil
+                        })).navigationTitle(info.title)
+                    }
+                }
+        }
+    }
+}
+
+
+// MARK: - Private Methods
+private extension HitRateCoordinatorView {
+    func updateVision(vision: Vision, info: StatsContainerInfo) {
+        switch info {
+        case .evasion:
+            dataModel.defender = vision
+        case .accuracy:
+            dataModel.attacker = vision
+        } 
+    }
+    
+    func showDetails(_ info: StatsContainerInfo, completion: ((Vision) -> Void)? = nil) -> some View {
+        let dataModel = makeDataModel(info, completion: completion)
+        
+        return VisionDetailsView(dataModel: dataModel)
+    }
+    
+    func makeDataModel(_ info: StatsContainerInfo, completion: ((Vision) -> Void)?) -> VisionDetailsDataModel {
+        switch info {
+        case .evasion(let vision):
+            return VisionDetailsDataModel(vision: vision, state: .evasion, completion: completion ?? { _ in })
+        case .accuracy(let vision):
+            return VisionDetailsDataModel(vision: vision, state: .accuracy, completion: completion ?? { _ in })
+        }
+    }
+}
+
+struct HitRateCoordinatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        HitRateCoordinatorView()
+    }
+}
--- HitRateCalc/UI/Views/HitRateView.swift
+++ HitRateCalc/UI/Views/HitRateView.swift
@@ -8,83 +8,45 @@
 import SwiftUI
 
 struct HitRateView: View {
-    @StateObject private var dataModel = HitRateDataModel()
-    @State private var selectedInfo: StatsContainerInfo?
+    @ObservedObject var dataModel: HitRateDataModel
     
     private var offset: CGFloat { getHeightPercent(20) }
     private var evasionOffset: CGFloat { dataModel.checkingHitRate ? offset : -offset }
     private var accuracyOffset: CGFloat { dataModel.checkingHitRate ? -offset : offset }
     
     var body: some View {
-        NavigationStack {
+        VStack {
             VStack {
-                VStack {
-                    ZStack {
-                        VisionStatsContainer(viewModel: .accuracy(dataModel.attacker), resetValues: dataModel.resetAttacker)
-                            .offset(y: accuracyOffset)
-                            .onTapGesture {
-                                selectedInfo = .accuracy(dataModel.attacker)
-                            }
-                        
-                        SwitchButton(action: { dataModel.checkingHitRate.toggle() })
-                        
-                        VisionStatsContainer(viewModel: .evasion(dataModel.defender), resetValues: dataModel.resetDefender)
-                            .offset(y: evasionOffset)
-                            .onTapGesture {
-                                selectedInfo = .evasion(dataModel.defender)
-                            }
-                    }
-                    .animation(.default, value: dataModel.checkingHitRate)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity) // prevents view from moving off-screen
-                }
-                
-                Spacer()
-                FinalResultView(title: dataModel.finalRateTitle, resultRate: dataModel.finalRate)
-            }
-            .navigationTitle("Hit-Rate Calc")
-            .navigationBarTitleDisplayMode(.inline)
-            .sheet(item: $selectedInfo) { info in
-                NavigationStack {
-                    showDetails(info) { updatedVision in
-                        switch info {
-                        case .evasion:
-                            dataModel.defender = updatedVision
-                        case .accuracy:
-                            dataModel.attacker = updatedVision
+                ZStack {
+                    VisionStatsContainer(viewModel: .accuracy(dataModel.attacker), resetValues: dataModel.resetAttacker)
+                        .offset(y: accuracyOffset)
+                        .onTapGesture {
+                            dataModel.editAttacker()
                         }
-                        
-                        self.selectedInfo = nil
-                    }.navigationTitle(info.title)
+                    
+                    SwitchButton(action: { dataModel.checkingHitRate.toggle() })
+                    
+                    VisionStatsContainer(viewModel: .evasion(dataModel.defender), resetValues: dataModel.resetDefender)
+                        .offset(y: evasionOffset)
+                        .onTapGesture {
+                            dataModel.editDefender()
+                        }
                 }
-                
+                .animation(.default, value: dataModel.checkingHitRate)
+                .frame(maxWidth: .infinity, maxHeight: .infinity) // prevents view from moving off-screen
             }
+            
+            Spacer()
+            FinalResultView(title: dataModel.finalRateTitle, resultRate: dataModel.finalRate)
         }
     }
 }
 
-
-// MARK: - Subviews
-private extension HitRateView {
-    func showDetails(_ info: StatsContainerInfo, completion: ((Vision) -> Void)? = nil) -> some View {
-        let dataModel = makeDataModel(info, completion: completion)
-        
-        return VisionDetailsView(dataModel: dataModel)
-    }
-    
-    func makeDataModel(_ info: StatsContainerInfo, completion: ((Vision) -> Void)?) -> VisionDetailsDataModel {
-        switch info {
-        case .evasion(let vision):
-            return VisionDetailsDataModel(vision: vision, state: .evasion, completion: completion ?? { _ in })
-        case .accuracy(let vision):
-            return VisionDetailsDataModel(vision: vision, state: .accuracy, completion: completion ?? { _ in })
-        }
-    }
-}
 
 
 // MARK: - Preview
 struct HitRateView_Previews: PreviewProvider {
     static var previews: some View {
-        HitRateView()
+        HitRateView(dataModel: HitRateDataModel())
     }
 }
