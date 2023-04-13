139c34d1b8d900b782dacdf843f42b9c16cb30ba
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-13 08:59:38 -0700

    added logic to switch focusedState to proper TextField for each state

--- HitRateCalc/UI/Views/VisionDetailsView.swift
+++ HitRateCalc/UI/Views/VisionDetailsView.swift
@@ -8,27 +8,42 @@
 import SwiftUI
 
 struct VisionDetailsView: View {
+    @FocusState private var focusedIndex: Int?
     @StateObject var dataModel: VisionDetailsDataModel
     
     var body: some View {
         Form {
             Section {
                 TraitTextField("Name", text: $dataModel.name, keyboardType: .alphabet)
+                    .focused($focusedIndex, equals: 0)
             }.onlyShow(when: dataModel.showNameField)
             
             Section {
                 TraitTextField("Luck", text: $dataModel.luck)
+                    .focused($focusedIndex, equals: 1)
             }
             
             Section("Evasion") {
                 TraitTextField("Agility", text: $dataModel.agility)
+                    .focused($focusedIndex, equals: 2)
                 TraitTextField("Evasion", text: $dataModel.evasion)
+                    .focused($focusedIndex, equals: 3)
             }.onlyShow(when: dataModel.showEvasion)
             
             Section("Accuracy") {
                 TraitTextField("Dexterity", text: $dataModel.dexterity)
+                    .focused($focusedIndex, equals: 4)
                 TraitTextField("Accuracy", text: $dataModel.accuracy)
+                    .focused($focusedIndex, equals: 5)
             }.onlyShow(when: dataModel.showAccuracy)
+        }
+        .bindFocus(focusState: _focusedIndex, publishedFocusState: $dataModel.focusedIndex)
+        .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Button(action: { focusedIndex = nil }, label: { Text("Cancel") }).tint(.primary)
+                Spacer()
+                Button(dataModel.nextButtonText, action: dataModel.nextButtonAction)
+            }
         }
     }
 }
@@ -81,6 +96,7 @@ final class VisionDetailsDataModel: ObservableObject {
     @Published var dexterity: String
     @Published var evasion: String
     @Published var accuracy: String
+    @Published var focusedIndex: Int?
     
     private let originalVision: Vision
     private let state: VisionDetailState
@@ -100,10 +116,55 @@ final class VisionDetailsDataModel: ObservableObject {
 extension VisionDetailsDataModel {
     var showNameField: Bool { state ==  .allDetails }
     var showEvasion: Bool { state != .accuracy }
-    var showAccuracy: Bool { state != .evasion}
+    var showAccuracy: Bool { state != .evasion }
+    var nextButtonText: String {
+        guard let focusedIndex = focusedIndex else { return "" }
+        
+        return focusedIndex < finalFieldIndex ? "Next" : "Done"
+    }
+    
+    func nextButtonAction() {
+        guard let focusedIndex = focusedIndex else { return }
+        guard let currentIndex = availableFieldIndices.firstIndex(where: { $0 == focusedIndex }) else { return }
+        
+        if currentIndex < (availableFieldIndices.count - 1) {
+            self.focusedIndex = availableFieldIndices[currentIndex + 1]
+        } else {
+            self.focusedIndex = nil
+        }
+    }
+}
+
+private extension VisionDetailsDataModel {
+    var finalFieldIndex: Int { state == .evasion ? 3 : 5 }
+    var availableFieldIndices: [Int] {
+        switch state {
+        case .allDetails: return [0, 1, 2, 3, 4, 5]
+        case .evasion: return [1, 2, 3]
+        case .accuracy: return [1, 4, 5]
+        }
+    }
 }
 
 
 enum VisionDetailState {
     case allDetails, evasion, accuracy
+}
+
+struct BindFocusStateViewModifier: ViewModifier {
+    @FocusState var focusState: Int?
+    @Binding var publishedFocus: Int?
+
+    
+    func body(content: Content) -> some View {
+        content
+            .onChange(of: focusState, perform: { publishedFocus = $0 })
+            .onChange(of: publishedFocus, perform: { focusState = $0 })
+    }
+}
+
+extension View {
+    func bindFocus(focusState: FocusState<Int?>, publishedFocusState: Binding<Int?>) -> some View {
+        modifier(BindFocusStateViewModifier(focusState: focusState, publishedFocus: publishedFocusState))
+    }
 }
