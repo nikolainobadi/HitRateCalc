7ea7481a94b5c0c21a42c0a949102a7e03e5fa1f
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-13 10:50:02 -0700

    minor reformatting in VisionStatsContainer

--- HitRateCalc/UI/Views/VisionStatsContainer.swift
+++ HitRateCalc/UI/Views/VisionStatsContainer.swift
@@ -12,54 +12,87 @@ struct VisionStatsContainer: View {
     
     var body: some View {
         VStack(spacing: 0) {
-            Text(viewModel.title)
+            StatHeader(title: viewModel.title, action: { })
+            
+            HStack {
+                StatList(statList: viewModel.statList)
+                Divider()
+                    .background(.primary)
+                    .frame(maxHeight: getHeightPercent(21))
+                StatRate(statRate: viewModel.statRate)
+            }.withRoundedBorder()
+        }
+    }
+}
+
+
+// MARK: - Header
+fileprivate struct StatHeader: View {
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+        HStack {
+            Text(title)
                 .font(.largeTitle.bold())
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.horizontal)
             
-            HStack {
-                VStack(spacing: 0) {
-                    ForEach(viewModel.statList) { stats in
-                        HStack {
-                            Text(stats.name)
-                                .lineLimit(1)
-                                .font(.title3.weight(.heavy))
-                                .minimumScaleFactor(0.5)
-                                .foregroundColor(Color(uiColor: .systemBackground))
-                                .padding(.horizontal, 5)
-                                .frame(maxWidth: getWidthPercent(25), maxHeight: .infinity, alignment: .center)
-                                .background(Color(uiColor: .label))
-                            
-                            TextField("0", text: .constant(stats.value))
-                                .font(.title)
-                                .keyboardType(.numberPad)
-                                .multilineTextAlignment(.center)
-                                .background(Color(uiColor: .secondarySystemBackground))
-                                .cornerRadius(10)
-                                .padding(10)
-                                .disabled(true)
-                        }.frame(maxHeight: getHeightPercent(7))
-                    }
-                }
-                Divider().frame(maxHeight: getHeightPercent(21)).background(.primary)
-                VStack {
-                    // MARK: - TODO
-                    // decide whether or not to include this button
-//                    Button(action: { }) {
-//                        Text("Clear values")
-//                            .underline()
-//                            .font(.caption)
-//                    }.padding(5)
-                    Spacer()
-                    Text("\(viewModel.statRate)%")
-                        .lineLimit(1)
-                        .font(.largeTitle)
-                        .minimumScaleFactor(0.5)
-                        .frame(maxWidth: getWidthPercent(25))
-                    Spacer()
-                }.frame(maxHeight: getHeightPercent(21))
-            }.withRoundedBorder()
+            Button("Set Unit", action: action)
+                .buttonStyle(.bordered)
+                .padding(.horizontal)
+                .padding(.vertical, 8)
         }
+    }
+}
+
+
+// MARK: - StatList
+fileprivate struct StatList: View {
+    let statList: [VisionStat]
+     
+    var body: some View {
+        VStack(spacing: 0) {
+            ForEach(statList) { stats in
+                HStack {
+                    Text(stats.name)
+                        .lineLimit(1)
+                        .font(.title3.weight(.heavy))
+                        .minimumScaleFactor(0.5)
+                        .foregroundColor(Color(uiColor: .systemBackground))
+                        .padding(.horizontal, 5)
+                        .frame(maxWidth: getWidthPercent(25), maxHeight: .infinity, alignment: .center)
+                        .background(Color(uiColor: .label))
+                    
+                    TextField("0", text: .constant(stats.value))
+                        .font(.title)
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.center)
+                        .background(Color(uiColor: .secondarySystemBackground))
+                        .cornerRadius(10)
+                        .padding(10)
+                        .disabled(true)
+                }.frame(maxHeight: getHeightPercent(7))
+            }
+        }
+    }
+}
+
+
+// MARK: - StatRate
+fileprivate struct StatRate: View {
+    let statRate: Int
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("\(statRate)%")
+                .lineLimit(1)
+                .font(.largeTitle)
+                .minimumScaleFactor(0.5)
+                .frame(maxWidth: getWidthPercent(25))
+            Spacer()
+        }.frame(maxHeight: getHeightPercent(21))
     }
 }
 
