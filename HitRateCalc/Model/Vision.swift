c86dffa378b5540b7df5e19be5b60495257316ee
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-13 08:15:07 -0700

    added new Vision struct

---
+++ HitRateCalc/Model/Vision.swift
@@ -0,0 +1,17 @@
+//
+//  Vision.swift
+//  HitRateCalc
+//
+//  Created by Nikolai Nobadi on 4/13/23.
+//
+
+import Foundation
+
+struct Vision {
+    var name: String = ""
+    var luck: Int = 0
+    var agility: Int = 0
+    var dexterity: Int = 0
+    var evasion: Int = 0
+    var accuracy: Int = 0
+}
