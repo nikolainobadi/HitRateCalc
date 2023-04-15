577a95b8b173b57ce10993a25e08defcd49ec410
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-14 17:36:18 -0700

    moved VisionStoreAdapter to its own file

--- HitRateCalc/DataModel/VisionDetailsDataModel.swift
+++ HitRateCalc/DataModel/VisionDetailsDataModel.swift
@@ -112,26 +112,3 @@ private extension VisionDetailsDataModel {
 protocol VisionStore {
     func saveVision(_ vision: Vision) async throws
 }
-
-final class VisionStoreAdapter {
-    private let store: VisionStore
-    private let completion: (Vision) -> Void
-    
-    init(store: VisionStore, completion: @escaping (Vision) -> Void) {
-        self.store = store
-        self.completion = completion
-    }
-}
-
-extension VisionStoreAdapter: VisionStore {
-    func saveVision(_ vision: Vision) async throws {        
-        if !vision.name.isEmpty {
-            try await store.saveVision(vision)
-        }
-        await finished(vision)
-    }
-}
-
-private extension VisionStoreAdapter {
-    @MainActor func finished(_ vision: Vision) { completion(vision) }
-}
